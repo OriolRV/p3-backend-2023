425eaf599efb143326c9f44d1156efb92e1a48e1
Oriol Rocabert <oriol.rocabert.valverde@gmail.com>
2023-06-05 22:45:11 +0200

    Created and tested PUT endpoint

--- src/endpoints.ts
+++ src/endpoints.ts
@@ -7,6 +7,8 @@ const router = Router();
 //Endpoints map
 //GET all [entity name] entries.
 //GET a specific [entity name] entry by ID.
+//POST a new [entity name] entry.
+//PUT a specific [entity name] entry's field by ID
 //DELETE a specific [entity name] entry by ID.
 
 //Store request's ID in requests's body
@@ -45,7 +47,7 @@ router.get(
 	})
 );
 
-//POST
+//POST endpoint for a new entry of a specific type.
 router.post(
 	"/",
 	errorChecked(async (req, res) => {
@@ -55,7 +57,18 @@ router.post(
 	})
 );
 
-//PUT
+//PUT endpoint for a specific field of a speific new entry of a specific type.
+router.put(
+	"/:id",
+	errorChecked(async (req: RequestWithId, res) => {
+		const target = req.originalUrl.split("/")[1];
+		const result = await prisma[target].update({
+			where: { id: Number(req.params.id) },
+			data: req.body,
+		});
+		res.status(200).json(result);
+	})
+);
 
 //DELETE endpoint for a specific entry of a specific entity type
 router.delete(
